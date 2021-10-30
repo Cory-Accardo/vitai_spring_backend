584ad439902e3957f7e665639be3d745e774e235
Cory Accardo <accardo@usc.edu>
2021-10-30 15:20:06 -0700

    first

---
+++ src/test/java/vitai/VitaiApplicationTests.java
@@ -0,0 +1,13 @@
+package vitai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
