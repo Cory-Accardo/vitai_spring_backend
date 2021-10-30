57a6a68994fbdf41d59124de9b87a279a2b5d100
Cory Accardo <accardo@usc.edu>
2021-10-30 16:04:43 -0700

    fix

--- src/main/java/ai/vit/alpha/HelloController.java
+++ src/main/java/ai/vit/alpha/HelloController.java
@@ -1,15 +1,17 @@
 package ai.vit.alpha;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/hello")
 @RestController
 public class HelloController {
 
-    public String hello(){
-        return "Good Job Cory, you did it!";
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "This means that you were successful cory!";
     }
 
 }
+
+
