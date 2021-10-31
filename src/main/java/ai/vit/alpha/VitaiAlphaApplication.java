de0e8ffe4514468ddfd7653dde003990e78c1a33
Cory Accardo <accardo@usc.edu>
2021-10-30 18:00:57 -0700

    context

--- src/main/java/ai/vit/alpha/VitaiAlphaApplication.java
+++ src/main/java/ai/vit/alpha/VitaiAlphaApplication.java
@@ -2,12 +2,21 @@ package ai.vit.alpha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class VitaiAlphaApplication {
+public class VitaiAlphaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VitaiAlphaApplication.class);
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(VitaiAlphaApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(VitaiAlphaApplication.class, args);
+		System.out.println("Running............");
 	}
 
 }
